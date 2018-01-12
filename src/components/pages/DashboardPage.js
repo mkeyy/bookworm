b05c00e6df91502eba149fd4f5c4ee155ddd2cd7
Michał Korman <michal.korman@o2.pl>
2018-01-12 13:33:17 +0100

    Authorised routes

---
+++ src/components/pages/DashboardPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import ConfirmedEmailMessage from './messages/ConfirmedEmailMessage';
+
+const DashboardPage = ({ isConfirmed }) => {
+  return <div>{!isConfirmed && <ConfirmedEmailMessage />}</div>;
+};
+
+DashboardPage.propTypes = {
+  isConfirmed: PropTypes.bool.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    isConfirmed: !!state.user.confirmed,
+  };
+}
+
+export default connect(mapStateToProps)(DashboardPage);
